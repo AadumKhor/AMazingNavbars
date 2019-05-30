b05769454e590c563f43ca2e3f3a2cc0d4e5174f
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-05-30 22:47:43 +0530

    Added new bars

---
+++ ClippedNavbars/singularity_navbar/android/app/src/main/java/com/example/singularity_navbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.singularity_navbar;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
